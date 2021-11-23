2f2cfb0e609855ef5155debbc9f5925d6a4874d2
Fabrice BONNET <bonnet_fabryce@yahoo.fr>
2021-11-23 09:50:41 +0100

    change dotfile by dotfiles

---
+++ polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+#for m in `xrandr --query | grep " connected"|cut -d " " -f1`; do
+#    MONITOR="$m" polybar -c ~/dotfiles/polybar/config.ini mybar &
+#done
+
+for m in `xrandr --query | grep " connected"|cut -d " " -f1`; do
+    MONITOR="$m" polybar -c ~/dotfiles/polybar/custom/config.ini main &
+done
+
+
+echo "Polybar launched..."
